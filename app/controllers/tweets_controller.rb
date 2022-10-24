b4294a40d86bf9762bf441cf3e2f2b243ce08bf3
Haven Schrock <havenao@gmail.com>
2022-10-23 20:13:55 -0400

    add form for selecting topic

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,8 +3,12 @@ class TweetsController < ApplicationController
 
   end
 
-  def tweets
-    response = HTTParty.get("http://localhost:3000/tweets", format: :plain) 
+  def get_tweets
+    response = HTTParty.get("https://fierce-lake-18710.herokuapp.com/tweets/#{params[:hashtag]}", format: :plain) 
     @tweets = JSON.parse response, symbolize_names: true
+  end
+
+  def choose_tag
+    # do I navigate from here?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "tweets#index"
 
-  get "/tweets", to: "tweets#tweets"
+  get "/tweets", to: "tweets#index"
+  get "/tweets/:hashtag", to: "tweets#get_tweets"
 end
