c30e31e7c2b93ccb146210fa0c455be0c861589a
Haven Schrock <havenao@gmail.com>
2022-10-24 10:13:36 -0400

    get tweets on form submit

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,14 +1,12 @@
 class TweetsController < ApplicationController
   def index
-
   end
 
   def get_tweets
-    response = HTTParty.get("https://fierce-lake-18710.herokuapp.com/tweets/#{params[:hashtag]}", format: :plain) 
+    hashtag = params[:hashtag]
+    
+    # Todo: validate that hashtag has value before executing
+    response = HTTParty.get("https://fierce-lake-18710.herokuapp.com/tweets/#{hashtag}", format: :plain) 
     @tweets = JSON.parse response, symbolize_names: true
-  end
-
-  def choose_tag
-    # do I navigate from here?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root "tweets#index"
 
   get "/tweets", to: "tweets#index"
-  get "/tweets/:hashtag", to: "tweets#get_tweets"
+  get "/get_tweets", to: "tweets#get_tweets"
 end
