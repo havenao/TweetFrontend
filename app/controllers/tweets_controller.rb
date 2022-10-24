b4bf0655e2ef1911392406819f200c88913c401f
Haven Schrock <havenao@gmail.com>
2022-10-24 14:33:48 -0400

    add environment variable for tweet_api route

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,7 +6,7 @@ class TweetsController < ApplicationController
     hashtag = params[:hashtag]
     
     # Todo: validate that hashtag has value before executing
-    response = HTTParty.get("https://fierce-lake-18710.herokuapp.com/tweets/#{hashtag}", format: :plain) 
+    response = HTTParty.get(Rails.application.credentials.dig(:tweet_api, :route) + "/#{hashtag}", format: :plain) 
     @tweets = JSON.parse response, symbolize_names: true
   end
 end
